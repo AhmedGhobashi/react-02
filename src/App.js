d68dd280c4ae58b07e7d45125f82147f96183b4e
AhmedGhobashi <ahmed.osamaa200000@gmail.com>
2026-06-08 15:00:11 +0300

    just useeffect and solving xss problem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,92 @@
-import logo from './logo.svg';
+
 import './App.css';
 import FirstUseEffect from './Components/FirstUseEffect/FirstUseEffect';
+import  {useEffect, useState} from 'react'; 
+import axios from 'axios';
 
 function App() {
+  const [term, setTerm] = useState ('');
+  const [result, setResult] = useState([]);
+
+  useEffect(()=>{
+    const search =async ()=>{
+
+      const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResult(respond.data.query.search);
+      console.log (respond.data.query.search);
+      
+    }  //search-by-axios closed
+      
+      if(term){
+      search();
+    }
+
+  }, [term]); // useEffect closed
+
+
+
+  const fetchingResult  = result.map ((el)=>{
+    return (
+      <tr key={el.pageid}>
+        <th scope="row">1</th>
+        <td>{el.title}</td>
+        <td> 
+          <span dangerouslySetInnerHTML={{'__html': el.snippet}} />
+        </td>
+      </tr>
+    );
+     
+  });
 
   return (
     <div className="App">
       <FirstUseEffect />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="my-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Search Input
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleFormControlInput1"
+                onChange = {(e)=>{
+                  setTerm (e.target.value)
+                }}
+                value = {term}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Desc</th>
+                </tr>
+              </thead>
+              <tbody>
+                {fetchingResult}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      );
     </div>
   );
 }
